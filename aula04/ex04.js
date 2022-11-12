70b609bbb467b97f0961852f59fbd094d5ac2aa3
QuidoBila <Kidobila007@gmail.com>
2022-11-12 04:25:36 +0000

    Initial commit

---
+++ aula04/ex04.js
@@ -0,0 +1,11 @@
+
+var idade = 18
+
+console.log(`Voce tem ${idade} anos`)
+if(idade < 18){
+    console.log('Voce pode votar')
+}else if(idade = 18){
+    console.log('Voto opcional')
+}else{
+    console.log('Voto obrigatorio')
+}
